bb8af27cb6aa36b6ea8203f00d63b8191609da9c
MarcCarbonellUOC <marccarbonell@uoc.edu>
2024-04-16 09:33:24 +0100

    Refactoring and Detail pages

--- src/discography.js
+++ src/discography.js
@@ -1,13 +1,10 @@
 const primaryColor = '#0A2647';
-const mercury = '#9c2227';
+const mercury2 = '#9c2227';
+const mercury1 = '#ae8957';
 const origins = '#346435';
 const evolve = '#142629';
 const smoke = '#17170f';
-const itunes = '#3c3333';
 const night = '#58555f';
-const continued = '#787186';
-const hell = '#343333';
-const imagine = '#3e2633';
 
 const cards = document.querySelectorAll('.card');
 
@@ -15,8 +12,11 @@ cards.forEach(card => {
   card.addEventListener('mouseover', function() {
     const cardImage = this.querySelector('.card_image');
     switch(cardImage.id) {
-      case 'mercury':
-        document.body.style.background = `radial-gradient(circle,  ${mercury} , black)`;
+      case 'mercury2':
+        document.body.style.background = `radial-gradient(circle,  ${mercury2} , black)`;
+        break;
+      case 'mercury1':
+        document.body.style.background = `radial-gradient(circle,  ${mercury1} , black)`;
         break;
       case 'origins':
         document.body.style.background = `radial-gradient(circle,  ${origins} , black)`;
@@ -27,20 +27,8 @@ cards.forEach(card => {
       case 'smoke':
         document.body.style.background = `radial-gradient(circle,  ${smoke} , black)`;
         break;
-      case 'itunes':
-        document.body.style.background = `radial-gradient(circle,  ${itunes} , black)`;
-        break;
       case 'night':
         document.body.style.background = `radial-gradient(circle,  ${night} , black)`;
-        break;
-      case 'continued':
-        document.body.style.background = `radial-gradient(circle,  ${continued} , black)`;
-        break;
-      case 'hell':
-        document.body.style.background = `radial-gradient(circle,  ${hell} , black)`;
-        break;
-      case 'imagine':
-        document.body.style.background = `radial-gradient(circle,  ${imagine} , black)`;
         break;
     }
     document.body.style.backgroundAttachment = 'fixed';
